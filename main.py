2dccbcbaa95ed4df19c7c95e32b079e91d993afe
Rayen <78444938+rayenkorbi@users.noreply.github.com>
2024-05-13 21:15:39 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+
+cv2.namedWindow('webcam')
+vc = cv2.VideoCapture(0)
+
+rval, frame1 = vc.read()
+rval, frame2 = vc.read()
+
+if(vc.isOpened()):
+    pass
+else:
+    rval = False
+
+while rval:
+    diff = cv2.absdiff(frame1, frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        if(cv2.contourArea(contour) < 5000):
+            continue
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame1, 'status: {}'.format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+    """
+    If you want this detector to work more precicely activate this bellow :
+    cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
+    """
+
+    cv2.imshow('webcam', frame1)
+    frame1 = frame2
+    ret, frame2 = vc.read()
+
+    key = cv2.waitKey(20)
+    if(key == 27):
+        break
+
+cv2.destroyWindow('webcam')
+
+#made by Rayen Korbi
